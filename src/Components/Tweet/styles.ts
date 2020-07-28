e3b9a8f681b04806e51ec2eeba997ccdc62ae352
PabloSSena <pablosilvasena@gmail.com>
2020-07-28 15:06:25 -0300

    Pronto pra celular

--- src/Components/Tweet/styles.ts
+++ src/Components/Tweet/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled,{css} from 'styled-components';
 import {Chat,Rocketseat,Favorite } from '../../Styles/icons';
 
 export const Container = styled.div`
@@ -62,15 +62,125 @@ export const Content = styled.div`
     flex-direction:column;
 
     width:100%;
+    margin-top:2px;
+    padding-left:59px;
 `;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+    display:flex;
+    align-items:center;
 
-export const Dot = styled.div``;
-export const Description = styled.div``;
-export const ImageContent = styled.div``;
-export const Icons = styled.div``;
-export const Status = styled.div``;
-export const CommentIcon = styled.div``;
-export const RetweetIcon = styled.div``;
-export const LikeIcon = styled.div``;
+    font-size:15px;
+    white-space:nowrap;
+
+    strong{
+        margin-right:5px;
+    };
+
+    span,time{
+        color:var(--gray);
+
+    }
+
+    strong,span{
+        white-space:nowrap;
+        text-overflow:ellipsis;
+        overflow:hidden;
+    }
+`;
+
+export const Dot = styled.div`
+    background:var(--gray);
+    width:2px;
+    height:2px;
+    margin:0 10px;
+`;
+
+export const Description = styled.p`
+    font-size:14px;
+    margin-top:4px;
+
+`;
+
+export const ImageContent = styled.div`
+    margin-top:12px;
+    width:100%;
+    height:min(285px,max(175px,41vw));
+
+    background:var(--gray);
+    border-radius:14px;
+
+    cursor:pointer;
+
+    :hover{
+        opacity:0.7;
+    }
+`;
+
+export const Icons = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    flex-wrap:wrap;
+    margin:11px auto 0;
+    width:100%;  /**Mobile */
+
+    @media(min-width:330px){
+        width:63%;
+    }
+
+    div{
+        cursor:pointer;
+
+        :hover{
+            opacity:0.7;
+        }
+    }
+
+
+`;
+
+export const Status = styled.div`
+    display:flex;
+    align-items:center;
+
+    font-size:14px;
+  
+
+    svg{
+        margin-right:5px;
+    }
+
+    &:nth-child(1){
+        &,svg path{
+            color:var(--gray);
+        }
+    }
+
+    &:nth-child(2){
+        color:var(--retweet);
+
+        svg path{
+            fill: var(--retweet);
+        }
+    }
+
+    &:nth-child(3){
+        color:var(--like);
+
+        svg path{
+             color:var(--like); 
+        }
+    }
+`;
+
+const iconCSS = css`
+    width:19px;
+    height:19px;
+`;
+
+export const CommentIcon = styled(Chat)`${iconCSS}`;
+
+export const RetweetIcon = styled(Rocketseat)`${iconCSS}`;
+
+export const LikeIcon = styled(Favorite)`${iconCSS}`;
